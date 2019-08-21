fea9d79961b1311d06fb2edf9f251855beb0b249
Chak Koppula <chak.koppula199@gmail.com>
2019-08-21 21:19:44 +0530

    modified the logger decorator and fixed some bugs in the tests regarding exception checking

--- BlackJack.py
+++ BlackJack.py
@@ -2,11 +2,16 @@ from UserDefinedExceptions import *
 
 class Card:
 
+	# list indexes used to determine what card is being used
 	SUIT = ["Clubs", "Diamonds", "Hearts", "Spades"]
+
+	# first 2 entries are None for convenience, so index 2 is 2, 3 is 3 etc
 	RANK = [None, None, "2", "3", "4", "5", "6", "7", 
 			"8", "9", "10", "Jack", "Queen", "King"]
 
+
 	def __init__(self, rank, suit):
+		# checks that card being created is valid
 		if (rank >= 2 and rank <= 13) and (suit <= 3 and suit >= 0):
 			self._rank = rank
 			self._suit = suit
@@ -14,8 +19,9 @@ class Card:
 			raise CardException(rank, suit)
 
 	def show(self):
+		"""Returns a string of the current card in an easy to read format"""
 		return "{} of {}".format(Card.RANK[self._rank], Card.SUIT[self._suit])
 
 if __name__ == "__main__":
-	card = Card(3, 2)
+	card = Card(2, 3)
 	card.show()
--- BlackJackTest.py
+++ BlackJackTest.py
@@ -1,25 +1,56 @@
 import BlackJack
 import unittest
 from UserDefinedExceptions import *
+import logging
+
+# decorator to wrap around tests that stores results and arguments and name of each test run in a text file
+def logger(test):
+	logging.basicConfig(filename="Test-Logs.txt", level=logging.INFO)
+
+	def wrapper(*args, **kwargs):
+		logging.info(" {} ran with args: {} and kwargs: {}".format(test.__name__, args, kwargs))
+
+		try:
+			result = test(*args, **kwargs)
+			logging.info(" {} RAN SUCCESSFULY\n".format(test.__name__))
+			return result
+		except AssertionError:
+			logging.info(" {} DID NOT RUN SUCCESSFULY\n".format(test.__name__))
+			raise AssertionError
+			
+		
+	return wrapper
+
 
 class CardTest(unittest.TestCase):
 
 	def setUp(self):
 		self.card = BlackJack.Card(8, 3)
 
+	@logger
 	def test_card_is_as_expected(self):
-		self.assertEqual(self.card.show(), "8 of Spades")
+		self.assertEqual(self.card.show(), "8 odf Spades")
 
+	@logger
 	def test_correct_show_format(self):
 		self.assertNotEqual(self.card.show(), "The 8 of Spades")
 		self.assertNotEqual(self.card.show(), "The Eight of Spades")
 		self.assertNotEqual(self.card.show(), "Eight of Spades")
 
-	def test_out_of_bounds_error(self):
+	@logger
+	def test_suit_out_of_bounds_error(self):
 		with self.assertRaises(CardException):
-			self.card = BlackJack.Card(4, 4) # suit out of range
-			self.card = BlackJack.Card(29, 2) # rank out of range
-			self.card = BlackJack.Card(39, 23) # rank and suit out of range
+			self.card = BlackJack.Card(4, 4)
+
+	@logger
+	def test_rank_out_of_bounds_error(self):
+		with self.assertRaises(CardException):
+			self.card = BlackJack.Card(2, 2)
+
+	@logger
+	def test_rank_and_suit_out_of_bounds_error(self):
+		with self.assertRaises(CardException):
+			self.card = BlackJack.Card(39, 23)
 
 
 
