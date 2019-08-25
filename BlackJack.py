979d6d84da542570db36c6c20d5104dc93eda0b2
Chak Koppula <chak.koppula199@gmail.com>
2019-08-25 22:35:39 +0530

    finished the main blackjack class, need to test before wrapping up the project

--- BlackJack.py
+++ BlackJack.py
@@ -8,12 +8,70 @@ class Blackjack:
 		self.user = User("user")
 		self.computer = Computer("computer")
 		self.drawpile = Deck("drawpile")
+		self.drawpile.shuffle()
+		self.game_over = False
 
 	def initial_deal(self):
-		pass
+		self.user.hand.add_card(self.drawpile.remove_card())
+		self.user.hand.add_card(self.drawpile.remove_card())
+		self.computer.hand.add_card(self.drawpile.remove_card())
+		self.computer.hand.add_card(self.drawpile.remove_card())
 
-	def user_turn(self):
-		pass
+	def users_turn(self):
+		is_bust = self.user.user_turn(self.drawpile)
+		if is_bust:
+			self.game_over = True
 
-	def computer_turn(self):
-		pass
+	def computers_turn(self):
+		is_bust = self.computer.computer_turn(self.drawpile)
+		if is_bust:
+			self.game_over = True
+
+	def show_scores(self):
+		self.user.display_data()
+		print()
+		self.computer.display_data()
+
+		user_bust = self.user.check_if_bust()
+		computer_bust = self.computer.check_if_bust()
+		if self.check_whos_bust(user_bust, computer_bust):
+			return
+
+		self.check_scores()
+
+	def check_whos_bust(self, user_bust, computer_bust):
+		if user_bust and computer_bust:
+			print("Both players bust!")
+			return True
+		elif user_bust and not computer_bust:
+			print("{} bust, {} wins".format(self.user.name, self.computer.name))
+			return True
+		elif not user_bust and computer_bust:
+			print("{} bust, {} wins".format(self.computer.name, self.user.name))
+			return True
+		return False
+
+	def check_scores(self):
+		if self.user.get_score() > self.computer.get_score():
+			print("{} wins!".format(self.user.name))
+		elif self.user.get_score() < self.computer.get_score():
+			print("{} wins!".format(self.computer.name))
+		else:
+			print("Its a draw!")
+
+
+	def play_game(self):
+		self.initial_deal()
+		while True:
+			self.users_turn()
+			self.computers_turn()
+			if self.game_over:
+				self.show_scores()
+				break
+			if self.user.stick and self.computer.stick:
+				self.show_scores()
+				break
+
+if __name__ == "__main__":
+	blackjack = Blackjack()
+	blackjack.play_game()
